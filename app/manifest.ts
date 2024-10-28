940860808619bca6736c80b2daf3d0a92d35b4f5
Amit Shinde <ashinde@spotify.com>
2024-10-28 22:14:08 +0100

    add logos

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,12 +13,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "favicon.ico",
+        src: "logo-96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+      {
+        src: "logo-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "favicon.ico",
+        src: "logo-512.png",
         sizes: "512x512",
         type: "image/png",
       },
