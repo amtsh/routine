b6cc9ec63b385ad17d32cce8fd6701efbb31c277
Amit Shinde <ashinde@spotify.com>
2024-11-10 01:08:32 +0100

    use pillmenu

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,12 @@ import Home from "./home/home";
 import { PlusIcon } from "lucide-react";
 
 import { MenuDrawer } from "./home/MenuDrawer";
+import PillMenu from "@/components/PillMenu";
+import { useState } from "react";
 
 export default function Index() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <main>
       <div className="min-h-screen bg-black text-white px-4 py-6 font-sans">
@@ -14,11 +18,18 @@ export default function Index() {
           <Link href="/new">
             <PlusIcon className="text-zinc-400" />
           </Link>
-
-          <MenuDrawer />
         </div>
         <Home />
       </div>
+
+      <PillMenu onMenuPress={() => setMenuOpen(true)} />
+
+      <MenuDrawer
+        isOpen={menuOpen}
+        onOpenChange={() => setMenuOpen(!menuOpen)}
+      />
+
+      <div className="my-24" />
     </main>
   );
 }
